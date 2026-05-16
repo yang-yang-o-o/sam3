f1d249b3d4cc56b44b2d17c77a715d4a52e00695
yang-yang-o-o <yang-yang-o-o.github.io>
2026-05-16 11:23:52 +0000

    add install.md and examples.py

---
+++ examples/sam3_image_predictor_example.py
@@ -0,0 +1,95 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sam3
+from PIL import Image
+from sam3 import build_sam3_image_model
+from sam3.model.box_ops import box_xywh_to_cxcywh
+from sam3.model.sam3_image_processor import Sam3Processor
+from sam3.visualization_utils import draw_box_on_image, normalize_bbox, plot_results
+
+sam3_root = os.path.join(os.path.dirname(sam3.__file__), "..")
+
+import torch
+
+# turn on tfloat32 for Ampere GPUs
+# https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+
+# use bfloat16 for the entire notebook
+torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+
+
+bpe_path = f"{sam3_root}/sam3/assets/bpe_simple_vocab_16e6.txt.gz"
+model = build_sam3_image_model(bpe_path=bpe_path, load_from_HF=False, checkpoint_path=f"{sam3_root}/checkpoint/sam3/sam3.pt")
+
+image_path = f"{sam3_root}/assets/images/test_image.jpg"
+image = Image.open(image_path)
+width, height = image.size
+processor = Sam3Processor(model, confidence_threshold=0.5)
+inference_state = processor.set_image(image)
+
+processor.reset_all_prompts(inference_state)
+inference_state = processor.set_text_prompt(state=inference_state, prompt="shoe")
+
+img0 = Image.open(image_path)
+fig = plot_results(img0, inference_state)
+fig.savefig("sam3_image_predictor_example_1.png")
+plt.close(fig)
+
+# # Here the box is in  (x,y,w,h) format, where (x,y) is the top left corner.
+# box_input_xywh = torch.tensor([480.0, 290.0, 110.0, 360.0]).view(-1, 4)
+# box_input_cxcywh = box_xywh_to_cxcywh(box_input_xywh)
+
+# norm_box_cxcywh = normalize_bbox(box_input_cxcywh, width, height).flatten().tolist()
+# print("Normalized box input:", norm_box_cxcywh)
+
+# processor.reset_all_prompts(inference_state)
+# inference_state = processor.add_geometric_prompt(
+#     state=inference_state, box=norm_box_cxcywh, label=True
+# )
+
+# img0 = Image.open(image_path)
+# image_with_box = draw_box_on_image(img0, box_input_xywh.flatten().tolist())
+# fig_box, ax_box = plt.subplots(figsize=(12, 8))
+# ax_box.imshow(image_with_box)
+# ax_box.axis("off")
+# fig_box.savefig("sam3_image_predictor_example_2_.png")
+# plt.close(fig_box)
+
+# fig = plot_results(img0, inference_state)
+# fig.savefig("sam3_image_predictor_example_2.png")
+# plt.close(fig)
+
+# box_input_xywh = [[480.0, 290.0, 110.0, 360.0], [370.0, 280.0, 115.0, 375.0]]
+# box_input_cxcywh = box_xywh_to_cxcywh(torch.tensor(box_input_xywh).view(-1,4))
+# norm_boxes_cxcywh = normalize_bbox(box_input_cxcywh, width, height).tolist()
+
+# box_labels = [True, False]
+
+# processor.reset_all_prompts(inference_state)
+
+# for box, label in zip(norm_boxes_cxcywh, box_labels):
+#     inference_state = processor.add_geometric_prompt(
+#         state=inference_state, box=box, label=label
+#     )
+
+# img0 = Image.open(image_path)
+# image_with_box = img0
+# for i in range(len(box_input_xywh)):
+#     if box_labels[i] == 1:
+#         color = (0, 255, 0)
+#     else:
+#         color = (255, 0, 0)
+#     image_with_box = draw_box_on_image(image_with_box, box_input_xywh[i], color)
+# fig_box, ax_box = plt.subplots(figsize=(12, 8))
+# ax_box.imshow(image_with_box)
+# ax_box.axis("off")
+# fig_box.savefig("sam3_image_predictor_example_3_.png")
+# plt.close(fig_box)
+# fig = plot_results(img0, inference_state)
+# fig.savefig("sam3_image_predictor_example_3.png")
+# plt.close(fig)
